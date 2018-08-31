10b55ec3cc99aa1fa771b3a10910eb6b857fc35d
Dirosaki <diegosgomes27@gmail.com>
2018-08-31 16:19:36 -0300

    animação e conteudo body

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let l1 = document.querySelector('.menu');
 let l2 = document.querySelector('.invisivel');
+let l3 = document.querySelector('.container');
+let l4 = document.querySelector('.container1');
+let l5 = document.querySelector('.logo');
+let l6 = document.querySelector('.container1>h3')
 
 l1.onclick = function(){
     l2.classList.toggle('mostrando');
+    l3.classList.toggle('container2');
+    l4.classList.toggle('container2');
+    l5.classList.toggle('logo1')
+
+}
+
+l6.addEventListener(onmouseover);{
+    
     
 }
